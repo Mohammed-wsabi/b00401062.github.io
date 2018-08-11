982d7254321797e75b3b71b7931ccb8bd0863fde
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-08-12 00:59:03 +0800

    Add files via upload

--- Packages/cgm/Metadata.py
+++ Packages/cgm/Metadata.py
@@ -19,4 +19,17 @@ class Metadata:
 		values = result.get("values", [])
 		return DataFrame(values, columns = values[header]).drop(header).set_index(values[header][index_col])
 	def load(self, header: int = 0, index_col: int = 0) -> DataFrame:
-		return read(header, index_col)
+		metadata = read(header, index_col)
+		metadata.set_index("檢體編號", inplace = True)
+		metadata.性別 = metadata.性別.astype("category")
+		metadata.出生日期 = metadata.出生日期.astype("datetime64[ns]")
+		metadata.本院最初診斷日 = metadata.本院最初診斷日.astype("datetime64[ns]")
+		metadata.診斷性及分期性手術處置日期 = metadata.診斷性及分期性手術處置日期.astype("datetime64[ns]")
+		metadata.首次治療日 = metadata.首次治療日.astype("datetime64[ns]")
+		metadata.首次手術日期 = metadata.首次手術日期.astype("datetime64[ns]")
+		metadata.放射治療開始日期 = metadata.放射治療開始日期.astype("datetime64[ns]")
+		metadata.放射治療結束日期 = metadata.放射治療結束日期.astype("datetime64[ns]")
+		metadata.化學治療開始日期 = metadata.化學治療開始日期.astype("datetime64[ns]")
+		metadata.最後聯絡或死亡日期 = metadata.最後聯絡或死亡日期.astype("datetime64[ns]")
+		metadata.生存狀態 = metadata.生存狀態.astype("category")
+		return metadata
