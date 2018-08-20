323c3a7477d83cc888c46aa8d49c2eb039afca6a
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-08-20 17:16:38 +0800

    Add files via upload

--- Packages/note/index.js
+++ Packages/note/index.js
@@ -6,7 +6,7 @@ function generate() {
 	let management = document.getElementById("management").value;
 	let complaint = document.getElementById("complaint").value;
 	let diagnosis = document.getElementById("diagnosis").value;
-	document.getElementById("output").innerHTML = `The patient is a ${year? year + "-year-" : ""}${month? month + "-month-" : ""}old ${gender}. ${capitalize(pronoun)} presents${management? " for " + management : ""} for ${complaint? complaint : ""}${complaint && diagnosis? " with " : ""}${diagnosis}.`;
+	document.getElementById("output").innerHTML = `The patient is a ${year? year + "-year-" : ""}${month? month + "-month-" : ""}old ${gender}. ${capitalize(pronoun)} presents${management? " for " : ""}${management}${complaint || diagnosis? " for " : ""}${complaint}${complaint && diagnosis? " with " : ""}${diagnosis}.`;
 }
 
 function capitalize(s) {
