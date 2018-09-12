3600f9a87759b1d5d316bc9d779188be08bfde47
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-12 22:01:00 +0800

    Add files via upload

--- Packages/cgm/Qiime.sh
+++ Packages/cgm/Qiime.sh
@@ -7,20 +7,20 @@ source activate qiime
 qiime tools import \
 	--type 'SampleData[PairedEndSequencesWithQuality]' \
 	--input-path $CGM/Manifest.csv \
-	--output-path $CGM/Subjects.qza \
-	--input-format ~/PairedEndFastqManifestPhred33
+	--output-path $CGM/Demux/Demux.qza \
+	--input-format PairedEndFastqManifestPhred33
 
 qiime demux summarize \
-	--i-data $CGM/Subjects.qza \
-	--o-visualization $CGM/Subjects.qzv
+	--i-data $CGM/Demux/Demux.qza \
+	--o-visualization $CGM/Demux/Demux.qzv
 
-qiime tools view $CGM/Subjects.qzv
+qiime tools view $CGM/Demux/Demux.qzv
 
 qiime dada2 denoise-single \
-	--i-demultiplexed-seqs $CGM/Subjects.qza \
+	--i-demultiplexed-seqs $CGM/Demux/Demux.qza \
 	--p-trim-left 0 \
 	--p-trunc-len 200 \
-	--o-representative-sequences $CGM/DADA/Representatives.qza \
+	--o-representative-sequences $CGM/DADA/Sequences.qza \
 	--o-table $CGM/DADA/Table.qza \
 	--o-denoising-stats $CGM/DADA/Stats.qza
 
