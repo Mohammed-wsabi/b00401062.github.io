69e4a5320087d48e824c22b8581a6e6f55185968
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-14 17:40:39 +0800

    Add files via upload

---
+++ Packages/rmatlab/elmat/eye.R
@@ -0,0 +1,20 @@
+eye <- function(n, m, typename = "double") {
+	stopifnot(class(typename) == "character")
+	stopifnot(typename %in% c("integer", "double"))
+	one <- ifelse(typename == "integer", 1L, 1)
+	if (missing(n)) {
+		return(one)
+	}
+	stopifnot(typeof(n) == "integer" && length(n) <= 2)
+	if (length(n) == 1) {
+		if (missing(m)) {
+			return(diag(one, n))
+		} else {
+			stopifnot(length(m) == 1)
+			return(diag(one, n, m))
+		}
+	} else if (length(n) == 2) {
+		stopifnot(missing(m))
+		return(diag(one, n[1], n[2]))
+	}
+}
