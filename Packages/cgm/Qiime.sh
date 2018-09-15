7207639754648378255de22a5a90ccf7782be0be
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-15 18:48:43 +0800

    Add files via upload

--- Packages/cgm/Qiime.sh
+++ Packages/cgm/Qiime.sh
@@ -42,7 +42,7 @@ qiime metadata tabulate \
 	--m-input-file $CGM/DenoisingStats.qza \
 	--o-visualization $CGM/DenoisingStats.qzv
 
-## Phylogeny
+## Phylogenetic Diversity Analyses
 
 qiime phylogeny align-to-tree-mafft-fasttree \
 	--i-sequences $CGM/Representative.qza \
@@ -62,6 +62,113 @@ qiime tools export \
 	--output-path $CGM
 
 mv $CGM/tree.nwk $CGM/RootedTree.nwk
+
+## Alpha and Beta Diversity Analysis
+
+qiime diversity core-metrics-phylogenetic \
+	--i-phylogeny rooted-tree.qza \
+	--i-table table.qza \
+	--p-sampling-depth 1109 \
+	--m-metadata-file sample-metadata.tsv \
+	--output-dir core-metrics-results
+
+qiime diversity alpha-group-significance \
+	--i-alpha-diversity core-metrics-results/faith_pd_vector.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--o-visualization core-metrics-results/faith-pd-group-significance.qzv
+
+qiime diversity alpha-group-significance \
+	--i-alpha-diversity core-metrics-results/evenness_vector.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--o-visualization core-metrics-results/evenness-group-significance.qzv
+
+qiime diversity beta-group-significance \
+	--i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--m-metadata-column BodySite \
+	--o-visualization core-metrics-results/unweighted-unifrac-body-site-significance.qzv \
+	--p-pairwise
+
+qiime diversity beta-group-significance \
+	--i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--m-metadata-column Subject \
+	--o-visualization core-metrics-results/unweighted-unifrac-subject-group-significance.qzv \
+	--p-pairwise
+
+qiime emperor plot \
+	--i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--p-custom-axes DaysSinceExperimentStart \
+	--o-visualization core-metrics-results/unweighted-unifrac-emperor-DaysSinceExperimentStart.qzv
+
+qiime emperor plot \
+	--i-pcoa core-metrics-results/bray_curtis_pcoa_results.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--p-custom-axes DaysSinceExperimentStart \
+	--o-visualization core-metrics-results/bray-curtis-emperor-DaysSinceExperimentStart.qzv
+
+## Alpha Rarefaction Plotting
+
+qiime diversity alpha-rarefaction \
+	--i-table table.qza \
+	--i-phylogeny rooted-tree.qza \
+	--p-max-depth 4000 \
+	--m-metadata-file sample-metadata.tsv \
+	--o-visualization alpha-rarefaction.qzv
+
+## Taxonomic Analysis
+
+qiime feature-classifier classify-sklearn \
+	--i-classifier gg-13-8-99-515-806-nb-classifier.qza \
+	--i-reads rep-seqs.qza \
+	--o-classification taxonomy.qza
+
+qiime metadata tabulate \
+	--m-input-file taxonomy.qza \
+	--o-visualization taxonomy.qzv
+
+qiime taxa barplot \
+	--i-table table.qza \
+	--i-taxonomy taxonomy.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--o-visualization taxa-bar-plots.qzv
+
+## Differential Abundance Testing
+
+qiime feature-table filter-samples \
+	--i-table table.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--p-where "BodySite='gut'" \
+	--o-filtered-table gut-table.qza
+
+qiime composition add-pseudocount \
+	--i-table gut-table.qza \
+	--o-composition-table comp-gut-table.qza
+
+qiime composition ancom \
+	--i-table comp-gut-table.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--m-metadata-column Subject \
+	--o-visualization ancom-Subject.qzv
+
+qiime taxa collapse \
+	--i-table gut-table.qza \
+	--i-taxonomy taxonomy.qza \
+	--p-level 6 \
+	--o-collapsed-table gut-table-l6.qza
+
+qiime composition add-pseudocount \
+	--i-table gut-table-l6.qza \
+	--o-composition-table comp-gut-table-l6.qza
+
+qiime composition ancom \
+	--i-table comp-gut-table-l6.qza \
+	--m-metadata-file sample-metadata.tsv \
+	--m-metadata-column Subject \
+	--o-visualization l6-ancom-Subject.qzv
+
+## Cleanup
 
 source deactivate
 
