7d96768aaf459b3ea0bbf06483d77ffba9a815d9
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-18 10:45:24 +0800

    Add files via upload

--- Packages/cgm/QIIME.sh
+++ Packages/cgm/QIIME.sh
@@ -9,7 +9,7 @@ source activate qiime
 
 qiime tools import \
 	--type "SampleData[PairedEndSequencesWithQuality]" \
-	--input-path $CGM/Manifest.csv \
+	--input-path $CGM/manifest.csv \
 	--input-format PairedEndFastqManifestPhred33 \
 	--output-path $CGM/demux.qza
 
@@ -37,7 +37,7 @@ qiime feature-table tabulate-seqs \
 qiime feature-table summarize \
 	--i-table $CGM/table.qza \
 	--o-visualization $CGM/table.qzv \
-	--m-sample-metadata-file $CGM/Metadata.tsv
+	--m-sample-metadata-file $CGM/metadata.tsv
 
 qiime metadata tabulate \
 	--m-input-file $CGM/denoising.qza \
@@ -70,7 +70,7 @@ qiime diversity core-metrics-phylogenetic \
 	--i-phylogeny $CGM/rooted-tree.qza \
 	--i-table $CGM/table.qza \
 	--p-sampling-depth 482 \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--output-dir $CGM/core-metrics-results
 
 ### Alpha Rarefaction
@@ -79,33 +79,33 @@ qiime diversity alpha-rarefaction \
 	--i-table $CGM/table.qza \
 	--i-phylogeny $CGM/rooted-tree.qza \
 	--p-max-depth 4784 \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--o-visualization $CGM/alpha-rarefaction.qzv
 
 ### Alpha Diversity
 
 qiime diversity alpha-group-significance \
 	--i-alpha-diversity $CGM/core-metrics-results/faith_pd_vector.qza \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--o-visualization $CGM/core-metrics-results/faith-pd-group-significance.qzv
 
 qiime diversity alpha-group-significance \
 	--i-alpha-diversity $CGM/core-metrics-results/evenness_vector.qza \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--o-visualization $CGM/core-metrics-results/evenness-group-significance.qzv
 
 ### Beta Diversity
 
 qiime diversity beta-group-significance \
 	--i-distance-matrix $CGM/core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--m-metadata-column Subject \
 	--o-visualization $CGM/core-metrics-results/unweighted-unifrac-subject-significance.qzv \
 	--p-pairwise
 
 qiime diversity beta-group-significance \
 	--i-distance-matrix $CGM/core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--m-metadata-column Tissue \
 	--o-visualization $CGM/core-metrics-results/unweighted-unifrac-tissue-significance.qzv \
 	--p-pairwise
@@ -124,14 +124,14 @@ qiime metadata tabulate \
 qiime taxa barplot \
 	--i-table $CGM/table.qza \
 	--i-taxonomy $CGM/taxonomy.qza \
-	--m-metadata-file $CGM/Metadata.tsv \
+	--m-metadata-file $CGM/metadata.tsv \
 	--o-visualization $CGM/taxa-bar-plots.qzv
 
 ## Differential Abundance Testing
 
 qiime feature-table filter-samples \
-	--i-table table.qza \
-	--m-metadata-file sample-metadata.tsv \
+	--i-table $CGM/table.qza \
+	--m-metadata-file $CGM/metadata.tsv \
 	--p-where "BodySite='gut'" \
 	--o-filtered-table gut-table.qza
 
