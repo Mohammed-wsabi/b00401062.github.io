262039900ce06914cde9e3b39a20e13d6e226c9f
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-27 09:16:25 +0800

    Add files via upload

--- Packages/gpa/index.js
+++ Packages/gpa/index.js
@@ -1,5 +1,10 @@
 function handle(files) {
-	const MAP = { "A+": 4.3, "A": 4, "A-": 3.7, "B+": 3.3, "B": 3, "B-": 2.7, "C+": 2.3, "C": 2, "C-": 1.7, "F": 0 };
+	const MAP = new Map(Object.entries({
+		"A+": 4.3, "A": 4, "A-": 3.7,
+		"B+": 3.3, "B": 3, "B-": 2.7,
+		"C+": 2.3, "C": 2, "C-": 1.7,
+		"F": 0
+	}));
 	file = files[0];
 	console.log(file.name);
 	var reader = new FileReader();
@@ -7,9 +12,9 @@ function handle(files) {
 		let text = new DOMParser().parseFromString(event.target.result, "text/html");
 		let rows = Array.from(text.querySelectorAll("body > table.style2 > tbody > tr"))
 			.filter(row =>["#DDDDDD", "#EEEEEE"].includes(row.getAttribute("bgcolor")))
-			.filter(row => Object.keys(MAP).includes(row.children[7].innerText.trim()));
+			.filter(row => MAP.has(row.children[7].innerText.trim()));
 		let sum = rows
-			.map(row => Number(row.children[6].innerText) * MAP[row.children[7].innerText.trim()])
+			.map(row => Number(row.children[6].innerText) * MAP.get(row.children[7].innerText.trim()))
 			.reduce((a, b) => a + b);
 		let credits = rows
 			.map(row => Number(row.children[6].innerText))
