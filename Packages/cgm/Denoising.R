ac9311ffe32cf2ed9a46401e6561f4f69f6748f4
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-27 13:38:52 +0800

    Add files via upload

--- Packages/cgm/Denoising.R
+++ Packages/cgm/Denoising.R
@@ -3,16 +3,18 @@ library(reticulate)
 
 Denoising <- R6Class(
 	classname = "Denoising",
+	private = list(
+		stats = NULL
+	),
 	public = list(
-		stats = NULL,
 		initialize = function(filepath) {
-			self$stats = read.table(filepath, header = TRUE, sep = "\t", row.names = 1)
-			use_python(system("which python3"))
+			private$stats = read.table(filepath, header = TRUE, sep = "\t", row.names = 1)
 		},
 		plot = function() {
+			use_python(system("which python3"))
 			pyplot <- import("matplotlib.pyplot")
-			for (i in 1:dim(self$stats)[1]) {
-				pyplot$plot(unlist(self$stats[i,]))
+			for (i in 1:dim(private$stats)[1]) {
+				pyplot$plot(unlist(private$stats[i, ]))
 			}
 			pyplot$yscale("log")
 			pyplot$ylabel("log(#Sequences)")
