2dd1c52e6a372c6150bd429a27c4a1000ad26165
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-09-27 13:45:06 +0800

    Update Denoising.R

--- Packages/cgm/Denoising.R
+++ Packages/cgm/Denoising.R
@@ -11,7 +11,7 @@ Denoising <- R6Class(
 			private$stats = read.table(filepath, header = TRUE, sep = "\t", row.names = 1)
 		},
 		plot = function() {
-			use_python(system("which python3"))
+			use_python(system("which python3", intern = TRUE))
 			pyplot <- import("matplotlib.pyplot")
 			for (i in 1:dim(private$stats)[1]) {
 				pyplot$plot(unlist(private$stats[i, ]))
