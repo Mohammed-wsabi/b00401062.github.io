e80dc10c755fe4c9687306ff88f3df06061f791b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-10-03 09:40:35 +0800

    Add files via upload

--- Packages/ratlab/R/general.R
+++ Packages/ratlab/R/general.R
@@ -1,7 +1,23 @@
 general <- list(
 	cd = setwd,
+	clearvars = rm,
+	dir = dir,
+	copyfile = file.copy,
+	getenv = Sys.getenv,
+	isfolder = dir.exists,
 	mkdir = dir.create,
+	movefile = file.rename,
 	onCleanup = on.exit,
+	path = searchpaths,
 	pwd = getwd,
-	rmdir = unlink
+	rmdir = unlink,
+	setenv = Sys.setenv,
+	system = function(command) system(command, intern = TRUE),
+	ver = function() version,
+	which = environment,
+	whos = function() data.frame(
+		Name = ls(envir = .GlobalEnv),
+		Bytes = ls(envir = .GlobalEnv) %>% Map(f = get) %>% Map(f = object.size) %>% unlist,
+		Class = ls(envir = .GlobalEnv) %>% Map(f = get) %>% Map(f = typeof) %>% unlist
+	)
 )
