a5e713aeed1cf6f0f1ae454ca55cc3bdaa8c272c
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-10-06 12:08:10 +0800

    Add files via upload

--- Packages/cgm/Denoising.sh
+++ Packages/cgm/Denoising.sh
@@ -2,24 +2,13 @@
 
 qiime dada2 denoise-paired \
 	--i-demultiplexed-seqs $CGM/demux.qza \
-	--p-trim-left-f 0 \
-	--p-trim-left-r 0 \
-	--p-trunc-len-f 120 \
-	--p-trunc-len-r 120 \
+	--p-trunc-len-f 0 \
+	--p-trunc-len-r 0 \
+	--p-trunc-q 20 \
 	--p-n-threads 0 \
-	--o-table $CGM/table-raw.qza \
-	--o-representative-sequences $CGM/rep-seqs-raw.qza \
+	--o-table $CGM/table.qza \
+	--o-representative-sequences $CGM/rep-seqs.qza \
 	--o-denoising-stats $CGM/denoising.qza
-
-qiime feature-table filter-features \
-	--i-table $CGM/table-raw.qza \
-	--p-min-frequency 2 \
-	--o-filtered-table $CGM/table.qza
-
-qiime feature-table filter-seqs \
-	--i-data $CGM/rep-seqs-raw.qza \
-	--i-table $CGM/table.qza \
-	--o-filtered-data $CGM/rep-seqs.qza
 
 qiime feature-table summarize \
 	--i-table $CGM/table.qza \
