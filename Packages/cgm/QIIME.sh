5fca76870dc07359a4279f778f1fd3796425921d
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-10-06 14:23:24 +0800

    Add files via upload

--- Packages/cgm/QIIME.sh
+++ Packages/cgm/QIIME.sh
@@ -5,14 +5,10 @@ export CGM=~/Downloads/Researches/CGM/Datasets
 
 source activate qiime
 
-./Documents/Packages/cgm/Importing.sh
-./Documents/Packages/cgm/Denoising.sh
-./Documents/Packages/cgm/Phylogeny.sh
-./Documents/Packages/cgm/Diversity.sh
-./Documents/Packages/cgm/Classifier.sh
-./Documents/Packages/cgm/Taxonomy.sh
-./Documents/Packages/cgm/ANCOM.sh
-./Documents/Packages/cgm/Gneiss.sh
+for step in $@
+do
+	./Documents/Packages/cgm/$step.sh
+done
 
 source deactivate
 
