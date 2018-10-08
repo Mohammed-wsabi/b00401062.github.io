e8fa7a38bb8de49ae31dc97d9ef084494cc132ca
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-10-08 13:01:10 +0800

    Add files via upload

--- Packages/cgm/ANCOM.sh
+++ Packages/cgm/ANCOM.sh
@@ -8,20 +8,23 @@ qiime composition ancom \
 	--i-table $DATASETS/composition-add-pseudocount/comp-table.qza \
 	--m-metadata-file $DATASETS/metadata/metadata.tsv \
 	--m-metadata-column Tissue \
-	--o-visualization $DATASETS/visualization/composition-ancom-tissue.qzv
+	--o-visualization $DATASETS/visualization/composition-ancom-tissue/composition-ancom-tissue.qzv
 
-qiime taxa collapse \
-	--i-table $DATASETS/dada2-denoise-paired/table.qza \
-	--i-taxonomy $DATASETS/feature-classifier-classify-sklearn/taxonomy.qza \
-	--p-level 6 \
-	--o-collapsed-table $DATASETS/taxa-collapse/table-l6.qza
+for l in {1..7}
+do
+	qiime taxa collapse \
+		--i-table $DATASETS/dada2-denoise-paired/table.qza \
+		--i-taxonomy $DATASETS/feature-classifier-classify-sklearn/taxonomy.qza \
+		--p-level $l \
+		--o-collapsed-table $DATASETS/taxa-collapse/table-l$l.qza
 
-qiime composition add-pseudocount \
-	--i-table $DATASETS/taxa-collapse/table-l6.qza \
-	--o-composition-table $DATASETS/composition-add-pseudocount/comp-table-l6.qza
+	qiime composition add-pseudocount \
+		--i-table $DATASETS/taxa-collapse/table-l$l.qza \
+		--o-composition-table $DATASETS/composition-add-pseudocount/comp-table-l$l.qza
 
-qiime composition ancom \
-	--i-table $DATASETS/composition-add-pseudocount/comp-table-l6.qza \
-	--m-metadata-file $DATASETS/metadata/metadata.tsv \
-	--m-metadata-column Tissue \
-	--o-visualization $DATASETS/visualization/composition-ancom-tissue-l6.qzv
+	qiime composition ancom \
+		--i-table $DATASETS/composition-add-pseudocount/comp-table-l$l.qza \
+		--m-metadata-file $DATASETS/metadata/metadata.tsv \
+		--m-metadata-column Tissue \
+		--o-visualization $DATASETS/visualization/composition-ancom-tissue/composition-ancom-tissue-l$l.qzv
+done
