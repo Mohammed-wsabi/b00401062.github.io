98d9347ad7ba3ec7e7706a1e326cf08fe34b4a12
Wen-Bin Luo <b00401062@ntu.edu.tw>
2018-12-10 15:48:30 +0800

    Add files via upload

--- Packages/nmv/Sample.py
+++ Packages/nmv/Sample.py
@@ -5,15 +5,23 @@ from pandas import read_csv
 from matplotlib.pyplot import *
 
 class Sample:
-	def load(self):
-		self.DF = read_csv("./Downloads/Projects/NMV/Datasets/Demographics.csv", index_col = 0)
+	@staticmethod
+	def load():
+		DF = read_csv("./Downloads/Projects/NMV/Datasets/Demographics.csv", index_col = 0)
 		with open("./Downloads/Projects/NMV/Datasets/Indices/GFA.pkl", "rb") as fin:
-			self.GFA = pickle.load(fin)
-		return self.DF, self.GFA
-	def plot(self):
-		hist(self.DF.Age, bins = range(18, 89))
+			GFA = pickle.load(fin)
+		return DF, GFA
+	@staticmethod
+	def plot(DF):
+		hist(DF.Age, bins = range(18, 89))
 		xlabel("Age")
 		ylabel("Frequency")
 		yticks(range(20))
 		savefig("./Downloads/Projects/NMV/Figures/Sample")
 		show()
+	@staticmethod
+	def dump(SCORES, QUANTILES):
+		with open("./Downloads/Projects/NMV/Datasets/Scores.pkl", "wb") as fout:
+			pickle.dump(SCORES, fout, pickle.HIGHEST_PROTOCOL)
+		with open("./Downloads/Projects/NMV/Datasets/Quantiles.pkl", "wb") as fout:
+			pickle.dump(QUANTILES, fout, pickle.HIGHEST_PROTOCOL)
--- Packages/nmv/__main__.py
+++ Packages/nmv/__main__.py
@@ -5,28 +5,33 @@ from sklearn.model_selection import KFold
 from pandas import DataFrame
 
 if __name__ == "__main__":
-	DF, GFA = Sample().load()
+	DF, GFA = Sample.load()
+	Sample.plot(DF)
 	Normality().fit(DF, GFA).plot()
 	MODELS = [PE(), LLSR(), QLSR(), GPR()]
+	SCORES = DataFrame(
+		index = [repeat(arange(76), 4), tile([model.__class__.__name__ for model in MODELS], 76)],
+		columns = scores.columns,
+		dtype = "float"
+	)
+	QUANTILES = DataFrame(
+		index = [repeat(arange(76), 4), tile([model.__class__.__name__ for model in MODELS], 76)],
+		columns = range(1, 4),
+		dtype = "float"
+	)
 	x = DF.Age
 	for i in range(76):
 		tract = TRACTS[i].Nickname
 		y = GFA[i].mean(axis = 0)
-		scores = DataFrame(
-			[DataFrame(cross_validate(
-				model, x, y,
-				cv = KFold(5, True),
-				scoring = ("r2", "neg_mean_squared_error"),
-				return_train_score = True
-			)).mean(axis = 0) for model in MODELS],
-			index = [model.__class__.__name__ for model in MODELS]
-		)
-		quantiles = DataFrame(
-			[Diagnostics(tract, model).quantile(x, y) for model in MODELS],
-			columns = range(1, 4),
-			index = [model.__class__.__name__ for model in MODELS]
-		)
+		SCORES.loc[i] = [DataFrame(cross_validate(
+			model, x, y,
+			cv = KFold(5, True),
+			scoring = ("r2", "neg_mean_squared_error"),
+			return_train_score = True
+		)).mean(axis = 0) for model in MODELS]
+		QUANTILES.loc[i] = [Diagnostics(tract, model).quantile(x, y) for model in MODELS]
 		for model in MODELS:
 			Diagnostics(tract, model.fit(x, y)).metrics(x, y)
 			Diagnostics(tract, model.fit(x, y)).scatter(x, y)
 			Diagnostics(tract, model.fit(x, y)).residual(x, y)
+	Sample.dump(SCORES, QUANTILES)
