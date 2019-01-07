e78d461040aea75e8240581126725580d4a67cb2
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-01-08 00:09:41 +0800

    Add files via upload

---
+++ Packages/sdp/Dataset.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from numpy import *
+from pandas import read_csv
+from pandas import DataFrame
+from re import compile
+from scipy.io import loadmat
+
+class Dataset:
+	@staticmethod
+	def get(index):
+		assert index in INDICES
+		raw = loadmat("/Users/luo/Downloads/Projects/SDP/Datasets/all_array_{}.mat".format(index.lower()))
+		values = array(list(map(lambda x: x.flatten(), raw["array"][0])))
+		pattern = "F:\\\\[^\\\\]+\\\\[^\\\\]+\\\\([^\\\\]+)\\\\.*"
+		folders = array(list(map(compile(pattern).findall, map(lambda x: x[0][0], raw["index_name"])))).flatten()
+		dataset = {"training": {"X": None, "y": None}, "test": {"X": None, "y": None}}
+		for set in Set._fields:
+			file = "/Users/luo/Downloads/Projects/SDP/Datasets/{}.csv".format(set.capitalize())
+			metadata = read_csv(file, index_col = 0)
+			mask = isin(folders, metadata.index)
+			dataset[set]["X"] = DataFrame(values[mask], index = folders[mask]).dropna(axis = 1)
+			dataset[set]["y"] = metadata.final[folders[mask]].astype("category")
+		return Set(training = Data(**dataset["training"]), test = Data(**dataset["test"]))
--- Packages/sdp/__main__.py
+++ Packages/sdp/__main__.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
 
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RationalQuadratic
+from sklearn.metrics import confusion_matrix
+
+if __name__ == "__main__":
+	DATASET = Dataset.get("GFA")
+	model = GaussianProcessClassifier(kernel = RationalQuadratic())
+	confusion_matrix(DATASET.test.y, model.fit(DATASET.training.X, DATASET.training.y.tolist()).predict(DATASET.test.X))
