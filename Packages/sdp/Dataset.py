f520d6c7b8675ba83df36a5c3ad7304f094e24da
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-01-08 15:41:47 +0800

    Add files via upload

--- Packages/sdp/Dataset.py
+++ Packages/sdp/Dataset.py
@@ -10,13 +10,13 @@ class Dataset:
 	@staticmethod
 	def get(index):
 		assert index in INDICES
-		raw = loadmat("/Users/luo/Downloads/Projects/SDP/Datasets/all_array_{}.mat".format(index.lower()))
+		raw = loadmat("./Downloads/Projects/SDP/Datasets/all_array_{}.mat".format(index.lower()))
 		values = array(list(map(lambda x: x.flatten(), raw["array"][0])))
 		pattern = "F:\\\\[^\\\\]+\\\\[^\\\\]+\\\\([^\\\\]+)\\\\.*"
 		folders = array(list(map(compile(pattern).findall, map(lambda x: x[0][0], raw["index_name"])))).flatten()
 		dataset = {"training": {"X": None, "y": None}, "test": {"X": None, "y": None}}
 		for set in Set._fields:
-			file = "/Users/luo/Downloads/Projects/SDP/Datasets/{}.csv".format(set.capitalize())
+			file = "./Downloads/Projects/SDP/Datasets/{}.csv".format(set.capitalize())
 			metadata = read_csv(file, index_col = 0)
 			mask = isin(folders, metadata.index)
 			dataset[set]["X"] = DataFrame(values[mask], index = folders[mask]).dropna(axis = 1)
--- Packages/sdp/__main__.py
+++ Packages/sdp/__main__.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
+from pandas import DataFrame
+from sklearn.decomposition import PCA
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import GridSearchCV
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RationalQuadratic
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
+from matplotlib.pyplot import *
 
 if __name__ == "__main__":
 	DATASET = Dataset.get("GFA")
+	## Dimensionality reduction
+	plot(cumsum(PCA().fit(DATASET.training.X).explained_variance_ratio_))
+	grid(axis = "y")
+	xlabel("# Feature")
+	ylabel("Percentage")
+	savefig("./Downloads/Projects/SDP/Figures/PCA")
+	show()
+	reducer = PCA(4).fit(DATASET.training.X)
+	## MLPClassifier
+	validator = GridSearchCV(
+		MLPClassifier(hidden_layer_sizes = (10,), max_iter = 10000),
+		{"alpha": 2. ** arange(-4, 4, .1)},
+		cv = LeaveOneOut()
+	).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
+	DataFrame(validator.cv_results_).to_csv("./Downloads/Projects/SDP/Datasets/Validator.csv")
+	model = MLPClassifier(hidden_layer_sizes = (10,), max_iter = 10000, alpha = 2. ** -2.2)
+	model = model.fit(reducer.transform(DATASET.training.X), DATASET.training.y.tolist())
+	confusion_matrix(DATASET.test.y, model.predict(reducer.transform(DATASET.test.X)))
+	## GaussianProcessClassifier
 	model = GaussianProcessClassifier(kernel = RationalQuadratic())
-	confusion_matrix(DATASET.test.y, model.fit(DATASET.training.X, DATASET.training.y.tolist()).predict(DATASET.test.X))
+	model = model.fit(DATASET.training.X, DATASET.training.y.tolist())
+	confusion_matrix(DATASET.test.y, model.predict(DATASET.test.X))
