7d4f75bb2e1ac2b495963df93485a0c60a408658
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-03-12 15:14:07 +0800

    Add files via upload

--- Packages/sdp/__main__.py
+++ Packages/sdp/__main__.py
@@ -10,37 +10,36 @@ from sklearn.gaussian_process.kernels import RationalQuadratic
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
-from matplotlib.pyplot import *
 from sdp.Constants import *
 from sdp.Dataset import *
 
 if __name__ == "__main__":
-DATASET = Dataset.get(["GFA", "RD"], [16, 17])
-## Dimensionality reduction
-reducer = PCA(.7).fit(DATASET.training.X)
-## SVC
-validator = GridSearchCV(
-	SVC(gamma = "scale"),
-	{"C": 2. ** arange(-10, 10)},
-	cv = LeaveOneOut()
-).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
-model = validator.best_estimator_
-## GaussianProcessClassifier
-model = GaussianProcessClassifier(kernel = RationalQuadratic())
-## RandomForestClassifier
-validator = GridSearchCV(
-	RandomForestClassifier(n_estimators = 100),
-	{"max_depth": range(2, 8)},
-	cv = LeaveOneOut()
-).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
-model = validator.best_estimator_
-## MLPClassifier
-validator = GridSearchCV(
-	MLPClassifier(hidden_layer_sizes = (int(sqrt(reducer.n_components_)),), solver = "lbfgs", max_iter = 10000),
-	{"alpha": 2. ** arange(-10, 10)},
-	cv = LeaveOneOut()
-).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
-model = validator.best_estimator_
-## Fitting
-model.fit(reducer.transform(DATASET.training.X), DATASET.training.y.tolist())
-confusion_matrix(DATASET.test.y, model.predict(reducer.transform(DATASET.test.X)))
+	DATASET = Dataset.get(["GFA", "RD"], [16, 17])
+	## Dimensionality reduction
+	reducer = PCA(.7).fit(DATASET.training.X)
+	## SVC
+	validator = GridSearchCV(
+		SVC(gamma = "scale"),
+		{"C": 2. ** arange(-10, 10)},
+		cv = LeaveOneOut()
+	).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
+	model = validator.best_estimator_
+	## GaussianProcessClassifier
+	model = GaussianProcessClassifier(kernel = RationalQuadratic())
+	## RandomForestClassifier
+	validator = GridSearchCV(
+		RandomForestClassifier(n_estimators = 100),
+		{"max_depth": range(2, 8)},
+		cv = LeaveOneOut()
+	).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
+	model = validator.best_estimator_
+	## MLPClassifier
+	validator = GridSearchCV(
+		MLPClassifier(hidden_layer_sizes = (int(sqrt(reducer.n_components_)),), solver = "lbfgs", max_iter = 10000),
+		{"alpha": 2. ** arange(-10, 10)},
+		cv = LeaveOneOut()
+	).fit(reducer.transform(DATASET.training.X), DATASET.training.y)
+	model = validator.best_estimator_
+	## Fitting
+	model.fit(reducer.transform(DATASET.training.X), DATASET.training.y.tolist())
+	confusion_matrix(DATASET.test.y, model.predict(reducer.transform(DATASET.test.X)))
