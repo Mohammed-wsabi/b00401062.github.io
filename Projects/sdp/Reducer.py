17ea8ee82c0f02a45e126b21fae9805f416ad599
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-03-17 15:38:25 +0800

    Update Reducer.py

--- Projects/sdp/Reducer.py
+++ Projects/sdp/Reducer.py
@@ -8,7 +8,8 @@ from matplotlib.pyplot import *
 class Reducer:
 	def fit(self, X, y):
 		param_grid = arange(.01, 1, .01)
-		score = list(map(lambda i: GaussianProcessClassifier().fit(PCA(i).fit_transform(X), y).log_marginal_likelihood_value_, param_grid))
+		classifier = GaussianProcessClassifier()
+		score = list(map(lambda i: classifier.fit(PCA(i).fit_transform(X), y).log_marginal_likelihood_value_, param_grid))
 		plot(range(1, 100), score)
 		xticks(linspace(0, 100, 11), list(map("{:.1f}".format, linspace(0, 1, 11))))
 		xlabel("Percentage")
