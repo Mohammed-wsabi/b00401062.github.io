38752c3289a7e4929756faf18bfc2bfcc04db3e0
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-03-29 23:52:32 +0800

    Add files via upload

--- Projects/dlc/Dataset.py
+++ Projects/dlc/Dataset.py
@@ -11,8 +11,8 @@ class Dataset:
 		if os.path.isfile("Labels.csv"):
 			LABELS = read_csv(self.DATASETDIR + "Labels.csv")
 		else:
-			id = os.listdir(self.PREPROCESSEDDIR)
-			labels = list(map(lambda f: f.split("_")[0], id))
-			LABELS = DataFrame({"ImageID": id, "Label": labels})
+			fs = os.listdir(self.PREPROCESSEDDIR)
+			labels = list(map(lambda f: f.split("_")[0], fs))
+			LABELS = DataFrame({"ImageID": fs, "Label": labels})
 			LABELS.to_csv(self.DATASETDIR + "Labels.csv", index = False)
 		return LABELS
