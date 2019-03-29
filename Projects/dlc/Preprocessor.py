5858ab8329bc467718699641552e1a3a1833ea17
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-03-29 23:27:19 +0800

    Add files via upload

--- Projects/dlc/Preprocessor.py
+++ Projects/dlc/Preprocessor.py
@@ -12,11 +12,11 @@ class Preprocesser:
 		self.S = S
 	def run(self):
 		for i, (f, minr, minc, maxr, maxc) in self.BOXES.iterrows():
-			if f.split("_")[1] is "IMG": continue
+			if f[0] == ".": continue
 			h = maxr - minr
 			w = maxc - minc
 			m = max(w, h)
 			X = imread(self.RAWDIR + f)[minr:maxr, minc:maxc]
 			X = pad(X, ((int((m - h)/2),) * 2, (int((m - w)/2),) * 2, (0,) * 2), mode = "constant", constant_values = 0)
 			X = resize(X, (self.S, self.S, 3))
-			imsave(self.PREPROCESSEDDIR + f[:-4], X)
+			imsave(self.PREPROCESSEDDIR + f[:-4] + str(minr) + str(minc), X)
