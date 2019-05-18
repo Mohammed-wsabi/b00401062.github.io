afd834617c747c73747a8d6212f8279dbd0c901b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-05-18 10:36:11 +0800

    Add files via upload

---
+++ Projects/codechef/SMRSTR.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class SMRSTR {
+	public static void main (String[] args) throws IOException {
+		Scanner stdin = new Scanner(System.in);
+		int t = stdin.nextInt();;
+		while (t-- > 0) {
+			int n = stdin.nextInt();
+			int q = stdin.nextInt();
+			stdin.nextLine();
+			long p = 1;
+			while (n-- > 0 && p <= (int) 1e9)
+				p *= stdin.nextInt();
+			stdin.nextLine();
+			while (q-- > 0)
+				System.out.printf("%d ", stdin.nextInt() / p);
+			System.out.println();
+		}
+	}
+}
