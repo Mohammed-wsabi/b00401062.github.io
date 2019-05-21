070a55934e416f7ad4b51563cb2a0d2829e871b5
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-05-21 08:44:20 +0800

    Add files via upload

---
+++ Projects/rosalind/INDC.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class INDC {
+	private static double choose(int N, int k) {
+		double x = 1;
+		for (int i = N; i > N - k; i--)
+			x *= i;
+		for (int i = k; i > 0; i--)
+			x /= i;
+		return x;
+	}
+	public static void main(String[] args) throws IOException {
+		int n = new Scanner(System.in).nextInt();
+		for (int k = 1; k <= 2 * n; k++) {
+			double p = 0;
+			for (int i = k; i <= 2 * n; i++)
+				p += choose(2 * n, i) * Math.pow(0.5, i) * Math.pow(0.5, 2 * n - i);
+			System.out.printf("%.3f\n", Math.log10(p));
+		}
+	}
+}
