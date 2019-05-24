b94c498695880386640b98de4f38706198af5593
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-05-24 22:53:19 +0800

    Add files via upload

--- Projects/codechef/BSTOPS.java
+++ Projects/codechef/BSTOPS.java
@@ -5,26 +5,29 @@ class BSTOPS {
 	private static class Node {
 		Node lt, rt;
 		int x;
-		Node(int x) { this.x = x; }
+		long p;
+		Node(int x, long p) {
+			this.x = x;
+			this.p = p;
+		}
 	}
-	static Node insert(Node root, int x, long p) {
+	private static Node insert(Node root, int x, long p) {
 		if (root == null) {
 			System.out.println(p);
-			return new Node(x);
+			return new Node(x, p);
 		} else if (x < root.x)
 			root.lt = insert(root.lt, x, p * 2);
 		else if (x > root.x)
 			root.rt = insert(root.rt, x, p * 2 + 1);
 		return root;
 	}
-	static Node delete(Node root, int x, long p) {
+	private static Node delete(Node root, int x) {
 		if (x < root.x)
-			root.lt = delete(root.lt, x, p * 2);
+			root.lt = delete(root.lt, x);
 		else if (x > root.x)
-			root.rt = delete(root.rt, x, p * 2 + 1);
+			root.rt = delete(root.rt, x);
 		else {
-			if (p > 0)
-				System.out.println(p);
+			System.out.println(root.p);
 			if (root.lt == null)
 				return root.rt;
 			else if (root.rt == null)
@@ -34,7 +37,7 @@ class BSTOPS {
 				while (successor.lt != null)
 					successor = successor.lt;
 				root.x = successor.x;
-				root.rt = delete(root.rt, successor.x, -1);
+				root.rt = delete(root.rt, successor.x);
 			}
 		}
 		return root;
@@ -42,16 +45,16 @@ class BSTOPS {
 	public static void main (String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		Node root = new Node(Integer.MIN_VALUE);
+		Node root = null;
 		while (n-- > 0) {
 			char c = in.next().charAt(0);
 			int x = in.nextInt();
 			switch (c) {
 			case 'i':
-				insert(root, x, 0);
+				root = insert(root, x, 1);
 				break;
 			case 'd':
-				delete(root, x, 0);
+				root = delete(root, x);
 				break;
 			}
 		}
