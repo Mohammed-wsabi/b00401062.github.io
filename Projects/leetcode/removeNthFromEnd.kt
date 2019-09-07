fe3fae246ee07cb6a723370f853da2f67d847478
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-09-07 17:41:58 +0800

    Add files via upload

---
+++ Projects/leetcode/removeNthFromEnd.kt
@@ -0,0 +1,12 @@
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var curr: ListNode? = head
+    var list = mutableListOf<ListNode>()
+    while (curr != null) {
+        list.add(curr)
+        curr = curr.next
+    }
+    if (list.size == n)
+        return head?.next
+    list[list.size - n - 1].next = list[list.size - n].next
+    return head
+}
