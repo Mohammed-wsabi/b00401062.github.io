80dca831650c521d6904d23cb478d3a8b136b76a
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-09-07 18:02:58 +0800

    Add files via upload

---
+++ Projects/leetcode/isValid.kt
@@ -0,0 +1,16 @@
+import java.util.Stack
+
+fun isValid(s: String): Boolean {
+    val stack = Stack<Char>()
+    for (c in s) {
+        when (c) {
+            '(' -> stack.push('(')
+            '[' -> stack.push('[')
+            '{' -> stack.push('{')
+            ')' -> if (stack.isEmpty() || stack.pop() != '(') return false
+            ']' -> if (stack.isEmpty() || stack.pop() != '[') return false
+            '}' -> if (stack.isEmpty() || stack.pop() != '{') return false
+        }
+    }
+    return stack.isEmpty()
+}
