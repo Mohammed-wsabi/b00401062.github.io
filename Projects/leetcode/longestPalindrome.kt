63085bd9898c828733bc1456c6de2a24bc8b70a7
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-09-14 23:02:35 +0800

    Add files via upload

--- Projects/leetcode/longestPalindrome.kt
+++ Projects/leetcode/longestPalindrome.kt
@@ -11,7 +11,7 @@ private fun check(s: String, _l: Int, _r: Int): String {
 fun longestPalindrome(s: String): String {
     var palindrome: String = ""
     for (i in s.indices) {
-        var candidates = listOf(
+        var candidates = arrayOf(
             palindrome,
             check(s, i-1, i+1),
             check(s, i, i+1)
