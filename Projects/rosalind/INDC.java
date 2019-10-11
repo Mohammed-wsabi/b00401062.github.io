ac7782b2ba64ea6aca75275f3fa9c63bf85e1ded
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-10-11 13:56:28 +0800

    Add files via upload

--- Projects/rosalind/INDC.java
+++ Projects/rosalind/INDC.java
@@ -1,22 +1,24 @@
+package rosalind;
+
 import java.io.*;
 import java.util.*;
 
 public class INDC {
-	private static double choose(int N, int k) {
-		double x = 1;
-		for (int i = N; i > N - k; i--)
-			x *= i;
-		for (int i = k; i > 0; i--)
-			x /= i;
-		return x;
-	}
-	public static void main(String[] args) throws IOException {
-		int n = new Scanner(System.in).nextInt();
-		for (int k = 1; k <= 2 * n; k++) {
-			double p = 0;
-			for (int i = k; i <= 2 * n; i++)
-				p += choose(2 * n, i) * Math.pow(0.5, 2 * n);
-			System.out.printf("%.3f\n", Math.log10(p));
-		}
-	}
+    private static double choose(int N, int k) {
+        double x = 1;
+        for (int i = N; i > N - k; i--)
+            x *= i;
+        for (int i = k; i > 0; i--)
+            x /= i;
+        return x;
+    }
+    public static void main(String[] args) throws IOException {
+        int n = new Scanner(System.in).nextInt();
+        for (int k = 1; k <= 2 * n; k++) {
+            double p = 0;
+            for (int i = k; i <= 2 * n; i++)
+                p += choose(2 * n, i) * Math.pow(0.5, 2 * n);
+            System.out.printf("%.3f\n", Math.log10(p));
+        }
+    }
 }
--- Projects/rosalind/INOD.java
+++ Projects/rosalind/INOD.java
@@ -1,9 +1,11 @@
+package rosalind;
+
 import java.io.*;
 import java.util.*;
 
 public class INOD {
-	public static void main(String[] args) throws IOException {
-		Scanner stdin = new Scanner(System.in);
-		System.out.println(stdin.nextInt()-2);
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner stdin = new Scanner(System.in);
+        System.out.println(stdin.nextInt()-2);
+    }
 }
