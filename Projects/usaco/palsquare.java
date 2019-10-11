8af770fb8522c16b69bd07a66bd9d2f20c2a9c55
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-10-11 14:10:10 +0800

    Add files via upload

--- Projects/usaco/palsquare.java
+++ Projects/usaco/palsquare.java
@@ -1,23 +1,19 @@
-/*
-ID: rombin82
-LANG: JAVA
-TASK: palsquare
-*/
+package usaco;
 
 import java.io.*;
 import java.util.*;
 
 public class palsquare {
-	public static void main(String[] args) throws IOException {
-		Scanner stdin = new Scanner(new File("palsquare.in"));
-		PrintWriter stdout = new PrintWriter(new File("palsquare.out"));
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> stdout.flush()));
-		int B = stdin.nextInt();
-		for (int i = 1; i <= 300; i++)
-			if (isPalindrome(Integer.toString(i * i, B)))
-				stdout.println(Integer.toString(i, B).toUpperCase() + " " + Integer.toString(i * i, B).toUpperCase());
-	}
-	private static boolean isPalindrome(String str) {
-		return new StringBuffer(str).reverse().toString().equals(str);
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner stdin = new Scanner(new File("palsquare.in"));
+        PrintWriter stdout = new PrintWriter(new File("palsquare.out"));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> stdout.flush()));
+        int B = stdin.nextInt();
+        for (int i = 1; i <= 300; i++)
+            if (isPalindrome(Integer.toString(i * i, B)))
+                stdout.println(Integer.toString(i, B).toUpperCase() + " " + Integer.toString(i * i, B).toUpperCase());
+    }
+    private static boolean isPalindrome(String str) {
+        return new StringBuffer(str).reverse().toString().equals(str);
+    }
 }
--- Projects/usaco/test.java
+++ Projects/usaco/test.java
@@ -1,17 +1,13 @@
-/*
-ID: rombin82
-LANG: JAVA
-TASK: test
-*/
+package usaco;
 
 import java.io.*;
 import java.util.*;
 
 public class test {
-	public static void main(String[] args) throws IOException {
-		Scanner stdin = new Scanner(new File("test.in"));
-		PrintWriter stdout = new PrintWriter(new File("test.out"));
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> stdout.flush()));
-		stdout.println(stdin.nextInt() + stdin.nextInt());
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner stdin = new Scanner(new File("test.in"));
+        PrintWriter stdout = new PrintWriter(new File("test.out"));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> stdout.flush()));
+        stdout.println(stdin.nextInt() + stdin.nextInt());
+    }
 }
