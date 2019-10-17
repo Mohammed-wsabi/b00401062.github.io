5cce0b85cf6a562bbd7490792bb2a32f28b48e8b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-10-17 22:36:51 +0800

    Add files via upload

--- Projects/ndpi/Utils.kt
+++ Projects/ndpi/Utils.kt
@@ -19,31 +19,31 @@ object Utils {
 
     @ExperimentalUnsignedTypes
     val TYPE2SIZE = arrayOf(
-            0,
-            UByte.SIZE_BYTES,
-            Char.SIZE_BYTES / 2,
-            UShort.SIZE_BYTES,
-            UInt.SIZE_BYTES,
-            UInt.SIZE_BYTES * 2,
-            Byte.SIZE_BYTES,
-            0,
-            Short.SIZE_BYTES,
-            Int.SIZE_BYTES,
-            Int.SIZE_BYTES * 2,
-            4,
-            8
+        0,
+        UByte.SIZE_BYTES,
+        Char.SIZE_BYTES / 2,
+        UShort.SIZE_BYTES,
+        UInt.SIZE_BYTES,
+        UInt.SIZE_BYTES * 2,
+        Byte.SIZE_BYTES,
+        0,
+        Short.SIZE_BYTES,
+        Int.SIZE_BYTES,
+        Int.SIZE_BYTES * 2,
+        4,
+        8
     )
 
     @ExperimentalUnsignedTypes
     val CLASS2SIZE: Map<KClass<*>, Int> = mapOf(
-            Char::class to Char.SIZE_BYTES / 2,
-            UByte::class to UByte.SIZE_BYTES,
-            UShort::class to UShort.SIZE_BYTES,
-            UInt::class to UInt.SIZE_BYTES,
-            Byte::class to Byte.SIZE_BYTES,
-            Short::class to Short.SIZE_BYTES,
-            Int::class to Int.SIZE_BYTES,
-            Float::class to Float.SIZE_BYTES,
-            Double::class to Double.SIZE_BYTES
+        Char::class to Char.SIZE_BYTES / 2,
+        UByte::class to UByte.SIZE_BYTES,
+        UShort::class to UShort.SIZE_BYTES,
+        UInt::class to UInt.SIZE_BYTES,
+        Byte::class to Byte.SIZE_BYTES,
+        Short::class to Short.SIZE_BYTES,
+        Int::class to Int.SIZE_BYTES,
+        Float::class to Float.SIZE_BYTES,
+        Double::class to Double.SIZE_BYTES
     )
 }
