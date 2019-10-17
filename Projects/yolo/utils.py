421866ab667d52f4d67b0ba1fb17f3df1c48ff59
Wen-Bin Luo <b00401062@ntu.edu.tw>
2019-10-17 22:43:44 +0800

    Add files via upload

--- Projects/yolo/utils.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from collections import namedtuple
-
-Set = namedtuple("Dataset", ["training", "test"])
-Variable = namedtuple("Variable", ["x", "y"])
-Shape = namedtuple("Shape", ["height", "width", "depth"])
