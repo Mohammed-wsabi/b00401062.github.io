0dfb553749228c1d0736eb8e0e397d1e7b7190ef
Neo Luo <b00401062@ntu.edu.tw>
2020-03-10 07:26:59 +0800

    Add files via upload

--- 專案/codechef/LAZER.java
+++ 專案/codechef/LAZER.java
@@ -12,35 +12,27 @@ class LAZER {
                 A[i] = reader.nextInt();
             }
             int[][] Q = new int[q][3];
-            int[] Y = new int[q];
+            TreeMap<Integer, List<Integer>> map = new TreeMap<>();
             for (int i = 0; i < q; i++) {
                 Q[i][0] = reader.nextInt();
                 Q[i][1] = reader.nextInt();
-                Q[i][2] = Y[i] = reader.nextInt();
-            }
-            Arrays.sort(Y);
-            List<Integer>[] lists = new List[q];
-            for (int i = 0; i < q; i++) {
-                lists[i] = new ArrayList<>();
+                Q[i][2] = reader.nextInt();
+                map.putIfAbsent(Q[i][2], new ArrayList<Integer>());
             }
             for (int i = 1; i < n; i++) {
                 int y1 = A[i - 1];
                 int y2 = A[i];
                 int minY = Math.min(y1, y2);
                 int maxY = Math.max(y1, y2);
-                int minYIdx = Arrays.binarySearch(Y, minY);
-                if (minYIdx < 0) minYIdx = -(minYIdx + 1);
-                int maxYIdx = Arrays.binarySearch(Y, maxY);
-                maxYIdx = (maxYIdx < 0) ? -(maxYIdx + 1) : (maxYIdx + 1);
-                for (int j = minYIdx; j < maxYIdx; j++) {
-                    lists[j].add(i);
+                for (List<Integer> list : map.subMap(minY, true, maxY, true).values()) {
+                    list.add(i);
                 }
             }
             for (int i = 0; i < q; i++) {
                 int x1 = Q[i][0];
                 int x2 = Q[i][1];
                 int y = Q[i][2];
-                List<Integer> list = lists[Arrays.binarySearch(Y, y)];
+                List<Integer> list = map.get(y);
                 int x1Idx = Collections.binarySearch(list, x1);
                 if (x1Idx < 0) x1Idx = -(x1Idx + 1);
                 int x2Idx = Collections.binarySearch(list, x2);
