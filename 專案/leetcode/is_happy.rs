7e432e654f37695e3b0d652460421770e913177a
Neo Luo <b00401062@ntu.edu.tw>
2020-04-03 12:31:55 +0800

    Add files via upload

---
+++ 專案/leetcode/is_happy.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+
+pub fn is_happy(n: i32) -> bool {
+    let mut curr = n;
+    let mut seen = HashSet::new();
+    while curr != 1 && !seen.contains(&curr){
+        seen.insert(curr);
+        let mut next = 0;
+        while curr != 0 {
+            let digit = curr % 10;
+            curr /= 10;
+            next += digit * digit;
+        }
+        curr = next;
+    }
+    return curr == 1;
+}
