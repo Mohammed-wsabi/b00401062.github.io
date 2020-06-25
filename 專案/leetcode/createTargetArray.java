054d26982cce7d4db44f5b28a09f96f22c904aa7
Neo Luo <b00401062@ntu.edu.tw>
2020-06-25 09:35:07 +0800

    Delete createTargetArray.java

--- 專案/leetcode/createTargetArray.java
+++
@@ -1,9 +0,0 @@
-package leetcode;
-
-public int[] CreateTargetArray(int[] nums, int[] index) {
-    List<Integer> res = new ArrayList<>();
-    for (int i = 0; i < nums.length; i++) {
-        res.add(index[i], nums[i]);
-    }
-    return res.stream().mapToInt(Integer::intValue).toArray();
-}
