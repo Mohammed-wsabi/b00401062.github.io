ab4cdf58c6e2e6eef9647874ace85aca1146061e
Neo Luo <b00401062@ntu.edu.tw>
2020-06-25 09:27:36 +0800

    Update createTargetArray.java

--- 專案/leetcode/createTargetArray.java
+++ 專案/leetcode/createTargetArray.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-public int[] createTargetArray(int[] nums, int[] index) {
+public int[] CreateTargetArray(int[] nums, int[] index) {
     List<Integer> res = new ArrayList<>();
     for (int i = 0; i < nums.length; i++) {
         res.add(index[i], nums[i]);
