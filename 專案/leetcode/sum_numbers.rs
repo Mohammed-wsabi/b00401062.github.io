a4c81e1a910438d32b7669310a454447379f72f7
Neo Luo <b00401062@ntu.edu.tw>
2020-06-28 10:38:00 +0800

    Add files via upload

--- 專案/leetcode/sum_numbers.rs
+++ 專案/leetcode/sum_numbers.rs
@@ -2,19 +2,20 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 pub fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    return traverse(&root, 0);
+    return traverse(root, 0);
 }
 
-fn traverse(root: &Option<Rc<RefCell<TreeNode>>>, root_sum: i32) -> i32 {
+fn traverse(root: Option<Rc<RefCell<TreeNode>>>, root_sum: i32) -> i32 {
     if root.is_none() {
         return 0;
     }
-    let node = root.as_ref().unwrap().borrow();
+    let node = root.unwrap();
+    let node = node.borrow();
     let curr_sum = root_sum * 10 + node.val;
     if node.left.is_none() && node.right.is_none() {
         return curr_sum;
     }
-    let left_sum = traverse(&node.left, curr_sum);
-    let right_sum = traverse(&node.right, curr_sum);
+    let left_sum = traverse(node.left.clone(), curr_sum);
+    let right_sum = traverse(node.right.clone(), curr_sum);
     return left_sum + right_sum;
 }
