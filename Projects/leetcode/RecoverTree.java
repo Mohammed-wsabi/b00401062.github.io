bcf4c1bd9162214035701895e373931341b948f1
Neo Luo <b00401062@ntu.edu.tw>
2020-11-02 12:23:47 +0800

    Add files via upload

--- Projects/leetcode/RecoverTree.java
+++ Projects/leetcode/RecoverTree.java
@@ -22,17 +22,13 @@ class RecoverTree {
     }
 
     public static void recoverTree(TreeNode root) {
-        TreeNode curNode = root;
         TreeNode preNode = new TreeNode(Integer.MIN_VALUE);
+        TreeNode curNode = root;
+        boolean pairIsFound = false;
         ArrayList<TreeNode> candNodes = new ArrayList<>();
         while (curNode != null) {
             if (curNode.left == null) {
-                if (curNode.val < preNode.val) {
-                    candNodes.add(preNode);
-                    candNodes.add(curNode);
-                }
-                preNode = curNode;
-                curNode = curNode.right;
+                pairIsFound = true;
             } else {
                 TreeNode tmpNode = curNode.left;
                 while (tmpNode.right != null && tmpNode.right != curNode) {
@@ -41,15 +37,19 @@ class RecoverTree {
                 if (tmpNode.right != curNode) {
                     tmpNode.right = curNode;
                     curNode = curNode.left;
+                    pairIsFound = false;
                 } else {
                     tmpNode.right = null;
-                    if (curNode.val < preNode.val) {
-                        candNodes.add(preNode);
-                        candNodes.add(curNode);
-                    }
-                    preNode = curNode;
-                    curNode = curNode.right;
+                    pairIsFound = true;
                 }
+            }
+            if (pairIsFound) {
+                if (curNode.val < preNode.val) {
+                    candNodes.add(preNode);
+                    candNodes.add(curNode);
+                }
+                preNode = curNode;
+                curNode = curNode.right;
             }
         }
         swapVal(candNodes.get(0), candNodes.get(candNodes.size() - 1));
