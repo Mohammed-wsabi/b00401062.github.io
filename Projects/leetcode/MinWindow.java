24e172f80cbf51211ef97c7814f4d34e581cac02
Wen-Bin Luo <b00401062@ntu.edu.tw>
2020-11-05 11:12:29 +0800

    Update MinWindow.java

--- Projects/leetcode/MinWindow.java
+++ Projects/leetcode/MinWindow.java
@@ -29,45 +29,45 @@ class MinWindow {
                 -1
             );
         }
-    }
-
-    private static Window initialWindow(String s, Map<Character, Integer> letterMap) {
-        int start = -1;
-        for (int i = 0; i < s.length(); i++) {
-            final char c = s.charAt(i);
-            if (!letterMap.containsKey(c) && start == -1) continue;
-            start = start == -1 ? i : start;
-            if (!letterMap.containsKey(c)) continue;
-            letterMap.put(c, letterMap.get(c) - 1);
-            if (letterMap.values().stream().anyMatch(x -> x > 0)) continue;
-            return new Window(start, i + 1);
+        
+        public static Window from(String s, Map<Character, Integer> letterMap) {
+            int start = -1;
+            for (int i = 0; i < s.length(); i++) {
+                final char c = s.charAt(i);
+                if (!letterMap.containsKey(c) && start == -1) continue;
+                start = start == -1 ? i : start;
+                if (!letterMap.containsKey(c)) continue;
+                letterMap.put(c, letterMap.get(c) - 1);
+                if (letterMap.values().stream().anyMatch(x -> x > 0)) continue;
+                return new Window(start, i + 1);
+            }
+            return null;
         }
-        return null;
-    }
 
-    private static void refineWindow(Window window, String s, Map<Character, Integer> letterMap) {
-        while (true) {
-            final char c = s.charAt(window.start);
-            int deficit = letterMap.getOrDefault(c, Integer.MAX_VALUE);
-            if (deficit == 0) return;
-            window.start++;
-            if (deficit > 0) continue;
-            letterMap.put(c, deficit + 1);
+        public void refine(String s, Map<Character, Integer> letterMap) {
+            while (true) {
+                final char c = s.charAt(this.start);
+                final int deficit = letterMap.getOrDefault(c, Integer.MAX_VALUE);
+                if (deficit == 0) return;
+                this.start++;
+                if (deficit > 0) continue;
+                letterMap.put(c, deficit + 1);
+            }
         }
     }
 
     public static String minWindow(String s, String t) {
         final Map<Character, Integer> letterMap = letterMap(t);
-        Window curWindow = initialWindow(s, letterMap);
+        Window curWindow = Window.from(s, letterMap);
         if (curWindow == null) return "";
-        refineWindow(curWindow, s, letterMap);
+        curWindow.refine(s, letterMap);
         Window minWindow = curWindow;
         for (int end = curWindow.end; end < s.length(); end++) {
             final char endC = s.charAt(end);
             if (!letterMap.containsKey(endC)) continue;
             letterMap.put(endC, letterMap.get(endC) - 1);
             curWindow = new Window(curWindow.start, end + 1);
-            refineWindow(curWindow, s, letterMap);
+            curWindow.refine(s, letterMap);
             minWindow = minWindow.compareTo(curWindow) == -1 ? minWindow : curWindow;
         }
         return s.substring(minWindow.start, minWindow.end);
