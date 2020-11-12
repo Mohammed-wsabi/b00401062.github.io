a086197a4176d5e24be25d815316cebdafa0d8ba
Wen-Bin Luo <b00401062@ntu.edu.tw>
2020-11-12 14:48:45 +0800

    Update MinDistance.java

--- Projects/leetcode/MinDistance.java
+++ Projects/leetcode/MinDistance.java
@@ -1,23 +1,38 @@
 package leetcode;
 
 class MinDistance {
-    public static int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        for (int i1 = 1; i1 <= word1.length(); i1++) {
-            dp[i1][0] = i1;
+    private class Cache {
+        int diag;
+        int dist;
+        
+        public void update(int diag, int dist) {
+            this.diag = diag;
+            this.dist = dist;
         }
-        for (int i2 = 1; i2 <= word2.length(); i2++) {
-            dp[0][i2] = i2;
+    }
+
+    public int minDistance(String word1, String word2) {
+        int[] dp = new int[word2.length() + 1];
+        for (int j = 0; j <= word2.length(); j++) {
+            dp[j] = j;
         }
-        for (int i1 = 0; i1 < word1.length(); i1++) {
-            for (int i2 = 0; i2 < word2.length(); i2++) {
-                char c1 = word1.charAt(i1), c2 = word2.charAt(i2);
-                dp[i1 + 1][i2 + 1] = Math.min(
-                    dp[i1][i2] + (c1 == c2 ? 0 : 1),
-                    Math.min(dp[i1][i2 + 1], dp[i1 + 1][i2]) + 1
+        Cache cache = new Cache();
+        for (int i = 0; i < word1.length(); i++) {
+            final char c1 = word1.charAt(i);
+            cache.update(dp[0], i + 1);
+            dp[0] = cache.dist;
+            for (int j = 0; j < word2.length(); j++) {
+                final char c2 = word2.charAt(j);
+                cache.update(
+                    dp[j + 1],
+                    Math.min(
+                        cache.diag + (c1 == c2 ? 0 : 1),
+                        Math.min(dp[j], dp[j + 1]) + 1
+                    )
                 );
+                dp[j + 1] = cache.dist;
             }
         }
-        return dp[word1.length()][word2.length()];
+        return dp[word2.length()];
     }
 }
