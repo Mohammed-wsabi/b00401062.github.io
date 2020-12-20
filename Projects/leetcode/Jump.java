c71f422110449aeeb738263b1dba73d3e1e67aca
Wen-Bin Luo <b00401062@ntu.edu.tw>
2020-12-20 16:34:10 +0800

    Update Jump.java

--- Projects/leetcode/Jump.java
+++ Projects/leetcode/Jump.java
@@ -1,20 +1,19 @@
 package leetcode;
 
-class Jump {
-    public int jump(int[] nums) {
+class MaxResult {
+    public static int maxResult(int[] nums, int k) {
         final int n = nums.length;
-        if (n == 1) return 0;
-        int curJump = 0;
-        int nextMaxReach = 0;
-        int currMaxReach = 0;
-        for (int i = 0; i < n; i++) {
-            nextMaxReach = Math.max(nextMaxReach, i + nums[i]);
-            if (nextMaxReach >= n - 1) break;
-            if (i == currMaxReach) {
-                curJump++;
-                currMaxReach = nextMaxReach;
+        final int[] cache = new int[n];
+        cache[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            cache[i] = Integer.MIN_VALUE;
+        }
+        for (int i = 1; i < n; i++) {
+            final int num = nums[i];
+            for (int j = 1; j <= Math.min(k, i); j++) {
+                cache[i] = Math.max(cache[i], num + cache[i - j]);
             }
         }
-        return curJump + 1;
+        return cache[n - 1];
     }
 }
