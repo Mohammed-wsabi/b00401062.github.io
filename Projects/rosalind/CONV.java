1dc0c5da7b71851088b22b1ce0f527b9a9b37166
Wen-Bin Luo <b00401062@ntu.edu.tw>
2021-02-01 00:16:50 +0800

    Add files via upload

--- Projects/rosalind/CONV.java
+++ Projects/rosalind/CONV.java
@@ -14,7 +14,10 @@ public class CONV {
                 String key = String.format("%.5f", s1 - s2);
                 counter.put(key, counter.getOrDefault(key, 0) + 1);
             }
-        counter.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(1).forEach(e -> {
+        counter.entrySet().stream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .limit(1)
+        .forEach(e -> {
             System.out.println(e.getValue());
             System.out.println(e.getKey());
         });
